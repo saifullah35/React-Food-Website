740e32419a726a91ccf1a7ccec3c95f8301ed91a
Saif <saif@Arifs-MBP-4.lan>
2024-01-31 12:33:04 -0500

    Managing state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,12 +2,17 @@ import React from "react";
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
     return (
         <View style={styles.backgroundStyle}>
-            <Feather name= "search" size={30} />
+            <Feather name= "search" styles={styles.iconStyle} />
             <TextInput 
+                autoCapitalize="none"
+                autoCorrect={false}
+                styles={styles.inputStyle}
                 placeholder="Search"
+                value={term}
+                onChangeText={newTerm => onTermChange(newTerm)}
             />
         </View>
     );
@@ -15,6 +20,7 @@ const SearchBar = () => {
 
 const styles = StyleSheet.create({
     backgroundStyle: {
+        marginTop: 10,
         backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5,
@@ -22,9 +28,13 @@ const styles = StyleSheet.create({
         flexDirection: 'row'
     },
     inputStyle: {
-        borderColor: 'black',
-        borderWidth: 1,
-        flex: 1
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15
     }
 });
 
