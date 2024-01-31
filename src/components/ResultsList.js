434bc9b9f23137df6ad81589e7623710cac12755
Saif <saif@Arifs-MBP-4.lan>
2024-01-31 15:26:33 -0500

    few more styling issues

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,13 +4,13 @@ import ResultsDetail from "./ResultsDetail";
 
 const ResultsList = ({ title, results }) => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text style={styles.titleStyle}>{title} </Text>
             <FlatList 
                 horizontal= {true}
                 data={results}
-                keyExtractor = {(result) => result.id}
-                renderItem= {({item}) => {
+                keyExtractor = { (result) => result.id}
+                renderItem= {( { item } ) => {
                     return <ResultsDetail result={item} />;
                 }}
             />
@@ -21,7 +21,12 @@ const ResultsList = ({ title, results }) => {
 const styles = StyleSheet.create({
     titleStyle: {
         fontSize: 18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5
+    },
+    container: {
+        marginBottom: 10
     }
 });
 
