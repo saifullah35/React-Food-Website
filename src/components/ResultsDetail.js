9bb95db2a950234f84f7878bba1bbf27ccfe959d
Saif <saif@Arifs-MBP-4.lan>
2024-01-31 15:15:43 -0500

    showing additional info

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,16 +1,26 @@
 import React from "react";
-import { View,Text,StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ResultsDetail = ({ result }) => {
     return (
         <View>
-            <Text>{result.name}</Text>
+            <Image style={styles.image} source={{ uri: result.image_url }} />
+            <Text style={styles.name}>{result.name}</Text>
+            <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    
+    image: {
+        width: 250,
+        height: 120,
+        borderRadius: 4
+    },
+    name: {
+        fontWeight: 'bold',
+        fontSize: 16
+    }
 });
 
 export default ResultsDetail;
